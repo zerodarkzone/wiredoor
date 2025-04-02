66d29c1ea18e108e29741d033acf6f534ffab0e8
Daniel Mesa <dmesa1994@gmail.com>
2025-04-01 22:52:20 -0400

    fix: change project structure

--- jest.config.ts
+++ jest.config.ts
@@ -82,7 +82,6 @@ const config: Config = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/$1',
-    '^@wiredoor/shared(.*)$': '<rootDir>/../shared/src$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
